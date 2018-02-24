644438fe5d6079aed4a702e06fddedb1be4d3b71
Lappy Mc Lap Face <ephillipsiii>
2018-02-23 18:45:26 -0700

    initial logic attempt

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//creating all of the variables
+var randomNumber = 0;
+var wins = 0;
+var losses = 0;
+var buttonNumber = [];
+var score = 0;
+
+var randomNumber = Math.floor(Math.random() * 120) + 19;
+console.log(randomNumber);
