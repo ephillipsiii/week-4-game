5c18a8451b4303bc1b39a8c1d6c1b75a7f9947e6
Lappy Mc Lap Face <ephillipsiii>
2018-02-25 12:06:53 -0700

    images and most variables defined

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,14 @@
 //creating all of the variables
-var randomNumber = 0;
+// var randomNumber = 0;
 var wins = 0;
 var losses = 0;
 var buttonNumber = [];
 var score = 0;
 
-var randomNumber = Math.floor(Math.random() * 120) + 19;
-console.log(randomNumber);
+var randomNumber = Math.floor(Math.random() * 99) + 20;
+
+//buttons - still need to edit the number range.
+var button1 = Math.floor(Math.random() * 12) + 1;
+var button2 = Math.floor(Math.random() * 12) + 1;
+var button3 = Math.floor(Math.random() * 12) + 1;
+var button4 = Math.floor(Math.random() * 12) + 1;
