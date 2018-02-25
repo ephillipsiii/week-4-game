d3246952288dc8e08c649e795335a027d4f55fac
Lappy Mc Lap Face <ephillipsiii>
2018-02-25 16:00:50 -0700

    buttons kind of work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,28 +7,43 @@ $( document ).ready(function(){
     var button1, button2, button3, button4;
     var currentGuess = 0;
     
-        randomNumber = Math.floor(Math.random() * 101) + 19;
-        $(".targetScore").append(randomNumber);
-        $("#wins").append(wins);
-        $("#losses").append(losses);
-        $("#score").append(currentGuess);
-        //buttons 
-        button1 = Math.floor(Math.random() * 12) + 1;
-        button2 = Math.floor(Math.random() * 12) + 1;
-        button3 = Math.floor(Math.random() * 12) + 1;
-        button4 = Math.floor(Math.random() * 12) + 1;
-        console.log(button1, button2, button3, button4);
-        }
-    );
     
-    // function winOrLose(){
-    //   if (currentGuess > randomNumber) {
-    //       losses++;
+    randomNumber = Math.floor(Math.random() * 101) + 19;
+    $(".targetScore").append(randomNumber);
+    $("#wins").append(wins);
+    $("#losses").append(losses);
+    $("#score").append(currentGuess);
+    //buttons 
+    button1 = Math.floor(Math.random() * 12) + 1;
+    button2 = Math.floor(Math.random() * 12) + 1;
+    button3 = Math.floor(Math.random() * 12) + 1;
+    button4 = Math.floor(Math.random() * 12) + 1;
+    console.log(button1, button2, button3, button4);
 
-    //   }  
-    // }
+    $("#button1").click(function(){
+        $(".currentGuess").append(currentGuess + + button1);
+    });
+    $("#button2").click(function(){
+        $(".currentGuess").append(currentGuess + + button2);
+    });
+    $("#button3").click(function(){
+        $(".currentGuess").append(currentGuess + + button3);
+    });
+    $("#button4").click(function(){
+        $(".currentGuess").append(currentGuess + + button4);
+    });
 
-// });
+}
+);
+
+    
+
+    
+    
+
+    
+
+
 
 
 
