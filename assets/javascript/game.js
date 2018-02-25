e3e8283ff8cfaddb528fc72d2b1051afb4957203
Lappy Mc Lap Face <ephillipsiii>
2018-02-25 16:26:11 -0700

    fully working buttons!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ $( document ).ready(function(){
     var button1, button2, button3, button4;
     var currentGuess = 0;
     
-    
+    //set initial numbers
     randomNumber = Math.floor(Math.random() * 101) + 19;
     $(".targetScore").append(randomNumber);
     $("#wins").append(wins);
@@ -20,18 +20,29 @@ $( document ).ready(function(){
     button4 = Math.floor(Math.random() * 12) + 1;
     console.log(button1, button2, button3, button4);
 
+    // detects clicks on the buttons, adds the numbers and edits to the screen
     $("#button1").click(function(){
-        $(".currentGuess").append(currentGuess + + button1);
+        currentGuess = currentGuess + button1;
+        console.log(currentGuess);
+        $(".currentGuess").html(currentGuess);
     });
     $("#button2").click(function(){
-        $(".currentGuess").append(currentGuess + + button2);
+        currentGuess = currentGuess + button2;
+        console.log(currentGuess);
+        $(".currentGuess").html(currentGuess);
     });
     $("#button3").click(function(){
-        $(".currentGuess").append(currentGuess + + button3);
+        currentGuess = currentGuess + button3;
+        console.log(currentGuess);
+        $(".currentGuess").html(currentGuess);
     });
     $("#button4").click(function(){
-        $(".currentGuess").append(currentGuess + + button4);
+        currentGuess = currentGuess + button4;
+        console.log(currentGuess);
+        $(".currentGuess").html(currentGuess);
     });
+
+
 
 }
 );
