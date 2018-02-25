947cebbb96e899007ed0553340f513816d960de4
Lappy Mc Lap Face <ephillipsiii>
2018-02-25 13:27:52 -0700

    attempt at formating with bootstrap

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,14 @@ var wins = 0;
 var losses = 0;
 var buttonNumber = [];
 var score = 0;
+var button1, button2, button3, button4;
 
-var randomNumber = Math.floor(Math.random() * 99) + 20;
+var randomNumber = Math.floor(Math.random() * 101) + 19;
+
 
 //buttons - still need to edit the number range.
 var button1 = Math.floor(Math.random() * 12) + 1;
 var button2 = Math.floor(Math.random() * 12) + 1;
 var button3 = Math.floor(Math.random() * 12) + 1;
 var button4 = Math.floor(Math.random() * 12) + 1;
+
